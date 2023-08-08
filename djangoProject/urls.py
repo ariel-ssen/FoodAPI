cdceef2cf1720943c7fb7a998d216803b013011b
ssen Ariel <qhdghl75@gmail.com>
2023-08-09 00:00:13 +0900

    filter botton making

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -3,14 +3,32 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from product.views import ProductListAPI, ProductViewSet, V1ExampleView
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Food API",
+        default_version='v1',
+        description="안녕하세요. 'Food' Open API 문서 페이지 입니다.",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="qhdghl75@gmail.com"),
+        license=openapi.License(name="SSEN"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 # Create a router and register the ProductViewSet
 router = DefaultRouter()
 router.register(r'products', ProductViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/product/', ProductListAPI.as_view(), name='product-list'),
+    path('api/products/', ProductListAPI.as_view(), name='product-list'),
     path('api/', include(router.urls)),
     path('v1/', V1ExampleView.as_view(), name='v1-version'), 
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),  # Swagger 문서 경로
+
 
 ]
--- product/views.py
+++ product/views.py
@@ -12,31 +12,79 @@ from rest_framework.filters import SearchFilter
 from .models import Product
 from .serializers import ProductSerializer
 
-class ProductListAPI(APIView):
-    def get(self, request):
-        queryset = Product.objects.all()
-        print(queryset)
-        serializer = ProductSerializer(queryset, many=True)
-        return Response(serializer.data)
+# 초반 라우팅 나누기전 code
+# class ProductListAPI(APIView):
+#     def get(self, request):
+#         queryset = Product.objects.all()
+#         print(queryset)
+#         serializer = ProductSerializer(queryset, many=True)
+#         return Response(serializer.data)
     
-
-
 # Product 모델과 시리얼라이저 정의
 class ProductViewSet(viewsets.ModelViewSet):
+    '''
+    Food 영양정보 CRUD
+    
+        ---
+        # 내용
+            id : 번호
+            food_cd : 식품코드
+            group_name : 식품군
+            food_name : 식품이름
+            research_year : 조사년도
+            maker_name : 지역/제조사
+            serving_size : 1회 제공량
+            calorie : 열량(kcal)(1회제공량당)
+            protein : 단백질(g)(1회제공량당)
+            province : 지방(g)(1회제공량당)
+            carbohydrate : 탄수화물(g)(1회제공량당)
+            sugars : 총당류(g)(1회제공량당)
+            salt : 나트륨(mg)(1회제공량당)
+            cholesterol : 콜레스테롤(mg)(1회제공량당)
+            saturated_fatty_acids : 포화지방산(g)(1회제공량당)
+            trans_fat : 트랜스지방(g)(1회제공량당)
+            ref_name : 자료출처
+    '''
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-# ProductListAPI 뷰 정의
+# create
 class ProductListAPI(generics.ListAPIView):
+    '''
+    Food 영양정보 CRUD
+    
+        ---
+        # 내용
+            id : 번호
+            food_cd : 식품코드
+            group_name : 식품군
+            food_name : 식품이름
+            research_year : 조사년도
+            maker_name : 지역/제조사
+            serving_size : 1회 제공량
+            calorie : 열량(kcal)(1회제공량당)
+            protein : 단백질(g)(1회제공량당)
+            province : 지방(g)(1회제공량당)
+            carbohydrate : 탄수화물(g)(1회제공량당)
+            sugars : 총당류(g)(1회제공량당)
+            salt : 나트륨(mg)(1회제공량당)
+            cholesterol : 콜레스테롤(mg)(1회제공량당)
+            saturated_fatty_acids : 포화지방산(g)(1회제공량당)
+            trans_fat : 트랜스지방(g)(1회제공량당)
+            ref_name : 자료출처
+    '''
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filter_backends = [SearchFilter]
-    search_fields = ['food_name', 'group_name', 'maker_name']
+    search_fields = ['food_cd', 'food_name', 'research_year', 'maker_name' ]
 
-# 기타 다른 뷰들이 있다면 여기에 추가 
-
-# 버전 1 예시 뷰 정의
+#  version 
 class V1ExampleView(APIView):
+    '''
+    현재 버전은 1 입니다. version.1
+    
+    
+    '''
     def get(self, request):
         data = {'message': 'This is API version 1'}
         return Response(data, status=status.HTTP_200_OK)
